82ecf75265a6142652f18c9c06e28be5ad3fc402
Gagun45 <163674163+Gagun45@users.noreply.github.com>
2024-09-19 12:15:32 +0300

    sms

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -24,7 +24,7 @@ export default function Board({ clearBoard, isCardVisible, toggleCard, boardHero
 
             <>
               <div key={hero.name}
-                className={`hero-item-board animate-opacity0to1 rounded-t-3xl flex flex-col items-center justify-start overflow-hidden flex-wrap p-[0.2rem] gap-[0.2rem] w-[12%] md:w-[14%] lg:w-[18%]
+                className={`hero-item-board animate-opacity0to1 rounded-t-3xl flex flex-col items-center justify-start overflow-hidden flex-wrap p-[0.2rem] gap-[0.2rem] w-[28%] md:w-[14%] lg:w-[18%]
                   ${hero.cost}`}
               >{hero.icon ? (<img key={uuidv4()} className='shadow-md shadow-black rounded-t-3xl w-full aspect-[2/1] cursor-pointer' onClick={() => deleteHero(hero)} src={hero.icon} />) : <span className='name'>{hero.name}</span>}
                 <div key={uuidv4()} className='flex my-auto gap-[0.2rem] flex-wrap justify-center w-full'>
